9daf18afdf95578a06e4b4e041899f344a072037
Alex <alexferreiraa20@gmail.com>
2022-08-31 12:14:18 -0300

    Exercícios resolvidos Alex Ferreira Ammal-A

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,16 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+
+const minhaString2 = minhaString.trim()
+
+console.log(minhaString)
+console.log(`Quantidade de caracteres antes da Trim: ${minhaString.length} `)
+console.log(`Quantidade de caracteres depois da Trim: ${minhaString2.length}`)
+console.log(minhaString2.replace("________" , "sticioso"))
+
+
+
+
+
+
+
+
--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,28 @@
-//Crie a const para a frase aqui
+const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
+const fraseNova = frase.replace("verde","rosa").replace("azul", "laranja")
+
+
+console.log("*Primeira parte do exercício: frase com aspas*")
+console.log(frase)
+
+console.log("*Segunda parte do exercício: trocar verde por rosa e azul por laranja*")
+console.log(fraseNova)
+
+console.log("*Terceira parte do exercício: verificar se tem verde e laranja*")
+console.log(fraseNova.includes("verde"))
+console.log(fraseNova.includes("laranja"))
+
+console.log("*Parte extra do exercício: adicionar o trecho da frase em letra maiúscula*")
+console.log(fraseNova.toUpperCase("mas não deixe o gato sair"))
+
+
+
+
+
+
+
+
+
+
+
+
