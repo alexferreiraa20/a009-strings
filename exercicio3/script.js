76ba420ebea692229ea55ba544d3626dc543ab84
Alex <alexferreiraa20@gmail.com>
2022-08-31 14:33:49 -0300

    ajustado

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,6 +1,6 @@
 const frase = "Jorge tem uma casa verde e com portão azul, com os dizeres: \"BOAS VINDAS, mas não deixe o gato sair\""
 const fraseNova = frase.replace("verde","rosa").replace("azul", "laranja")
-
+const trechoFrase = "mas não deixe o gato sair"
 
 console.log("*Primeira parte do exercício: frase com aspas*")
 console.log(frase)
@@ -13,7 +13,7 @@ console.log(fraseNova.includes("verde"))
 console.log(fraseNova.includes("laranja"))
 
 console.log("*Parte extra do exercício: adicionar o trecho da frase em letra maiúscula*")
-console.log(fraseNova.toUpperCase("mas não deixe o gato sair"))
+console.log(`Jorge tem uma casa rosa e com portão laranja, com os dizeres : "BOAS VINDAS, ${trechoFrase.toUpperCase()}`)
 
 
 
